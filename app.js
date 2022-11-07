1496cf43c1048806a634606ad5573a1eb1a381ea
AnandJNairGit <anandjnairit@gmail.com>
2022-11-07 23:36:41 +0530

    add morgan logger

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const productRoutes = require("./API/products");
 const orderRoutes = require("./API/orders");
 
+app.use(morgan("dev"));
+
+// routes that handle request
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 

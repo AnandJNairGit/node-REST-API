bd86a7482ece0ea191574748c3ad4f33741b38ba
AnandJNairGit <anandjnairit@gmail.com>
2022-11-07 20:13:37 +0530

    declare orders route

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 const productRoutes = require("./API/products");
+const orderRoutes = require("./API/orders");
 
-app.use('/products', productRoutes );
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
 
 module.exports = app;

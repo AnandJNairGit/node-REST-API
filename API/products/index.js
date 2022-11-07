23a9b8d70f472e604f11210acd771f3e872c65bb
AnandJNairGit <anandjnairit@gmail.com>
2022-11-08 01:06:13 +0530

    print the body in products post request

--- API/products/index.js
+++ API/products/index.js
@@ -6,7 +6,10 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-  res.status(200).json({ message: "handling POST request to products" });
+  console.log(req.body);
+  res
+    .status(200)
+    .json({ message: "handling POST request to products", createdProduct: req.body });
 });
 
 router.get("/:productID", (req, res, next) => {

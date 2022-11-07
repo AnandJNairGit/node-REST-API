3c296b56c46bba4adff452562537830898bb2d4d
AnandJNairGit <anandjnairit@gmail.com>
2022-11-08 00:00:16 +0530

    add error handling for invalid routes

--- app.js
+++ app.js
@@ -11,4 +11,16 @@ app.use(morgan("dev"));
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 
+//error handling
+app.use((req, res, next) => {
+  const error = new Error('not found');
+  console.log("the error-------->", error);
+  error.status=404;
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: { message: error.message } });
+});
+
 module.exports = app;

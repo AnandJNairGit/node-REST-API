15a794797000aa558d07d4ba7ea7e5023a45aeea
AnandJNairGit <anandjnairit@gmail.com>
2022-11-08 15:09:38 +0530

    work on middleware

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const productRoutes = require("./API/products");
 const orderRoutes = require("./API/orders");
 
 app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+const middleware = (req, res, next) => {
+    console.log("hiii middleware");
+    next();
+  };
+app.use(middleware);
 
 // routes that handle request
 app.use("/products", productRoutes);
@@ -16,13 +21,15 @@ app.use("/orders", orderRoutes);
 
 //error handling
 app.use((req, res, next) => {
-  const error = new Error('not found');
-  error.status=404;
+  const error = new Error("not found");
+  error.status = 404;
   next(error);
 });
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({ error: { message: error.message } });
 });
+
+
 
 module.exports = app;

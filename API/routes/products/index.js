a620b297e1c060de1c8bcb077182a94f8ae1b44d
AnandJNairGit <anandjnairit@gmail.com>
2022-11-09 17:14:43 +0530

    store product data in the database

---
+++ API/routes/products/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const Product = require("../../models/product");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({ message: "handling GET request to products" });
+});
+
+router.post("/", (req, res, next) => {
+  console.log(req.body);
+
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price,
+  });
+
+  product
+    .save()
+    .then((result) => {
+      console.log("the result is--------->", result);
+      res.status(200).json({
+        message: "handling POST request to products",
+        createdProduct: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+});
+
+router.get("/:productID", (req, res, next) => {
+  const id = req.params.productID;
+          
+  if (id == "anand") {
+    res.status(200).json({ message: "hey! this is anand" });
+  } else {
+    res.status(200).json({ message: "use products/anand route to find anand" });
+  }
+});
+
+module.exports = router;

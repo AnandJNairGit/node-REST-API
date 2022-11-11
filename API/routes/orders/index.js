c66ece4ac0be3195d43b7840332d7bd91b1ce1e4
AnandJNairGit <anandjnairit@gmail.com>
2022-11-11 14:30:37 +0530

    handle patch request

--- API/routes/orders/index.js
+++ API/routes/orders/index.js
@@ -52,6 +52,17 @@ router.get("/:orderID", async (req, res, next) => {
   res.status(200).json(order);
 });
 
+//HANDLE ORDER PATCH REQUEST BY ID
+router.patch("/:orderID", async (req, res, next) => {
+  try {
+    const id = req.params.orderID;
+    const order = await Order.findByIdAndUpdate(id, req.body);
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 router.delete("/:orderID", (req, res, next) => {
   const id = req.params.orderID;
   res.status(200).json({ message: `order ${id} deleted`, id: id });

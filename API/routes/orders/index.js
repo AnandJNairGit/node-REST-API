df6ac94e1b9c7e02fa139a739316f388c93a2c79
AnandJNairGit <anandjnairit@gmail.com>
2022-11-11 14:17:00 +0530

    handle post and get request of osers route

--- API/routes/orders/index.js
+++ API/routes/orders/index.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const Order = require("../../models/order");
 
-router.get("/", (req, res, next) => {
-  res.status(200).json({ message: "fetched orders" });
+router.get("/", async (req, res, next) => {
+  const orderList = await Order.find().select("_id product quantity");
+  console.log(orderList);
+  res.status(200).json({
+    count: orderList.length,
+    orders: orderList.map((item) => {
+      console.log("the item -------->", item);
+      return {
+        _id: item._id,
+        product: item.product,
+        quantity: item.quantity,
+        request: {
+          type: "GET",
+          url: `http://localhost:3000/orders/${item._id}`,
+        },
+      };
+    }),
+  });
 });
 
-router.post("/", (req, res, next) => {
-  res.status(200).json({ message: "order created" });
+router.post("/", async (req, res, next) => {
+  try {
+    const order = new Order({
+      _id: new mongoose.Types.ObjectId(),
+      product: req.body.productId,
+      quantity: req.body.quantity,
+    });
+    const createdOrder = await order.save();
+    res.status(200).json({ message: "order created", order: createdOrder });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.get("/:orderID", (req, res, next) => {

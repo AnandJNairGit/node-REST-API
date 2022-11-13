eec7feaff6cfc5daef36d7bf427fe422b55e1286
AnandJNairGit <anandjnairit@gmail.com>
2022-11-13 19:27:32 +0530

    convert password to hash

--- API/routes/users/index.js
+++ API/routes/users/index.js
@@ -3,13 +3,22 @@ const { default: mongoose } = require("mongoose");
 const Users = require("../../models/user");
 const router = express.Router();
 
-router.post("/signup", (req,res,next)=>{
+const bcrypt = require("bcrypt");
 
-  const user = new Users({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-   email:req.body.email,
-   password:req.body.password
+router.post("/signup", (req, res, next) => {
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      const user = new Users({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hash,
+      });
+      console.log("the user data is:------>", user);
+    }
   });
 });
+
 module.exports = router;
